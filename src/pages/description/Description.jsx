16624f3d6b659c7c2f9072565d598a2310bb9a89
Diego Iván Paez <82846493+DiegoIpaez@users.noreply.github.com>
2024-05-01 19:36:59 -0300

    [FIX] style corrections (#10)
    * centered profile photo view
    
    * The description page has been reorganized in its view.
    
    * Colors of incoming messages have been added.
    
    * The icon and title of the website have been corrected

--- src/pages/description/Description.jsx
+++ src/pages/description/Description.jsx
@@ -4,16 +4,21 @@ import styles from './description.module.css';
 const Description = () => {
   return (
     <div className={styles.descriptionContainer}>
-      <DescriptionIcon />
-      <h1 className={styles.title}>WhatsApp Web</h1>
-      <p className={styles.subtitle}>
-        Envía y recibe mensajes sin necesidad de tener tu teléfono conectado.
-        <br />
-        Usa WhatsApp en hasta 4 dispositivos vinculados y 1 teléfono a la vez.
-      </p>
-      <p className={styles.footerText}>
-        Tus mensajes personales están cifrados de extremo a extremo.
-      </p>
+      <div className={styles.content}>
+        <DescriptionIcon />
+        <h1 className={styles.title}>WhatsApp Web</h1>
+        <p className={styles.subtitle}>
+          Envía y recibe mensajes sin necesidad de tener tu teléfono conectado.
+          <br />
+          Usa WhatsApp en hasta 4 dispositivos vinculados y 1 teléfono a la vez.
+        </p>
+      </div>
+      <div className={styles.footer}>
+        <span><i className="bi bi-lock-fill"></i></span>
+        <p className={styles.footerText}>
+          Tus mensajes personales están cifrados de extremo a extremo.
+        </p>
+      </div>
     </div>
   );
 };
