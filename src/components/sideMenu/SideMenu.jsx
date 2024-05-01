0a7acc170e32404b0950d6d2becfc6dc4268880a
DiegoIpaez <diegoipaez2000@gmail.com>
2024-04-30 23:55:24 -0300

    chat layout added

--- src/components/sideMenu/SideMenu.jsx
+++ src/components/sideMenu/SideMenu.jsx
@@ -1,5 +1,14 @@
+import ChatIcon from '../ui/icon/ChatIcon';
+import styles from './sideMenu.module.css';
+
 const SideMenu = () => {
-  return <div>Side menu</div>;
+  return (
+    <div className={styles.menuContainer}>
+      <div className={styles.item}>
+        <ChatIcon isOutline={false} />
+      </div>
+    </div>
+  );
 };
 
 export default SideMenu;
--- src/constants/chats.contants.js
+++ src/constants/chats.contants.js
@@ -1,123 +1,101 @@
 export const CHAT_COLLECTION = [
   {
     id: 1,
-    name: 'Axel Paez',
+    name: 'Billy G.',
     profilePicture:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKZm11YejYfeVWUIBhyMvi4A9QsaseqYAM1UzoHo4Grg&s',
+      'https://assets.bbhub.io/dotorg/sites/64/2023/09/BillGates-Headshot-2022.jpg',
     lastMessage: {
-      message: 'JAJAJAJA',
+      message: 'saludos!',
       createdAt: new Date(),
     },
-    unreadMessages: 5,
+    unreadMessages: 3,
   },
   {
     id: 2,
-    name: 'Juan Paez',
+    name: 'Ricardo',
     profilePicture:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKZm11YejYfeVWUIBhyMvi4A9QsaseqYAM1UzoHo4Grg&s',
+      'https://kranear.com.ar/public/files/images/kranear-okupas-web.jpg',
     lastMessage: {
-      message: 'JAJAJAJA',
-      createdAt: new Date(),
+      message: 'tremendo pancho',
+      createdAt: new Date('2024-05-01'),
     },
     unreadMessages: 0,
   },
   {
     id: 3,
-    name: 'Jorge Paez',
+    name: 'Antonio S.',
     profilePicture:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKZm11YejYfeVWUIBhyMvi4A9QsaseqYAM1UzoHo4Grg&s',
+      'https://media.gq.com.mx/photos/5f2446fe51bcbdbc95b13533/4:3/w_2664,h_1998,c_limit/TONY.jpg',
     lastMessage: {
-      message: 'JAJAJAJA',
-      createdAt: new Date(),
+      message: 'vence hoy el plazo, anda con cuidadito',
+      createdAt: new Date('2024-04-30'),
     },
-    unreadMessages: 5,
+    unreadMessages: 0,
   },
   {
     id: 4,
-    name: 'Jorge Paez',
+    name: 'Radia Perlman',
     profilePicture:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKZm11YejYfeVWUIBhyMvi4A9QsaseqYAM1UzoHo4Grg&s',
+      'https://images.squarespace-cdn.com/content/v1/5c032b5536099b1c1ce28450/fff8dcba-3954-4ff2-8774-c24e1d4cba1f/Radia+Perlman.jpg',
     lastMessage: {
-      message: 'JAJAJAJA',
-      createdAt: new Date(),
+      message: 'We need a meeting with you now',
+      createdAt: new Date('2024-04-29'),
     },
-    unreadMessages: 5,
+    unreadMessages: 0,
   },
   {
     id: 5,
-    name: 'Jorge Paez',
+    name: 'Jonathan Leno',
     profilePicture:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKZm11YejYfeVWUIBhyMvi4A9QsaseqYAM1UzoHo4Grg&s',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkfgdUfmajwk8j3rUo63hv2MyG_Sc4kOs7Hc8XS0WFlQ&s',
     lastMessage: {
-      message: 'JAJAJAJA',
-      createdAt: new Date(),
+      message: 'paz...',
+      createdAt: new Date('2024-04-28'),
     },
-    unreadMessages: 5,
+    unreadMessages: 0,
   },
   {
     id: 6,
     name: 'Julian Zottola',
     profilePicture:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKZm11YejYfeVWUIBhyMvi4A9QsaseqYAM1UzoHo4Grg&s',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfrM9Hp0JiYyyDm8_2gbX2vqAcQd8p25zvc9lAheLhjw&s',
     lastMessage: {
       message: '.',
-      createdAt: new Date(),
+      createdAt: new Date('2024-04-21'),
     },
-    unreadMessages: 5,
+    unreadMessages: 0,
   },
   {
     id: 7,
-    name: 'Los pibes',
+    name: 'Gym',
     profilePicture:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKZm11YejYfeVWUIBhyMvi4A9QsaseqYAM1UzoHo4Grg&s',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_ueyoIblzq4miNcabGZ8thH9_AV1-lUTB_lP3Xc8RqQ&s',
     lastMessage: {
-      message: 'JAJAJAJA',
-      createdAt: new Date(),
+      message: 'd1',
+      createdAt: new Date('2024-04-21'),
     },
-    unreadMessages: 5,
+    unreadMessages: 0,
   },
   {
     id: 8,
     name: 'Los pibes 2',
     profilePicture:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKZm11YejYfeVWUIBhyMvi4A9QsaseqYAM1UzoHo4Grg&s',
+      'https://indiehoy.com/wp-content/uploads/2016/08/perras-on-the-beach.jpg',
     lastMessage: {
-      message: 'JAJAJAJA',
-      createdAt: new Date(),
+      message: 'ok!',
+      createdAt: new Date('2024-03-21'),
     },
-    unreadMessages: 5,
+    unreadMessages: 0,
   },
   {
     id: 9,
-    name: 'Los pibes 3: la verganza',
+    name: 'Office',
     profilePicture:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKZm11YejYfeVWUIBhyMvi4A9QsaseqYAM1UzoHo4Grg&s',
+      'https://e00-marca.uecdn.es/assets/multimedia/imagenes/2023/09/25/16956276926001.jpg',
     lastMessage: {
-      message: 'JAJAJAJA',
-      createdAt: new Date(),
+      message: 'perfecto!',
+      createdAt: new Date('2022-04-21'),
     },
-    unreadMessages: 5,
-  },
-  {
-    id: 10,
-    name: 'Spiderman',
-    profilePicture:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKZm11YejYfeVWUIBhyMvi4A9QsaseqYAM1UzoHo4Grg&s',
-    lastMessage: {
-      message: 'JAJAJAJA',
-      createdAt: new Date(),
-    },
-    unreadMessages: 5,
-  },
-  {
-    id: 11,
-    name: 'Goku',
-    profilePicture:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKZm11YejYfeVWUIBhyMvi4A9QsaseqYAM1UzoHo4Grg&s',
-    lastMessage: {
-      message: 'JAJAJAJA',
-      createdAt: new Date(),
-    },
-    unreadMessages: 5,
+    unreadMessages: 10,
   },
 ];
