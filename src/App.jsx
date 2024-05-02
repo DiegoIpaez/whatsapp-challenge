535c7a4edb4093e4f033dd56c96476c92b65efce
DiegoIpaez <diegoipaez2000@gmail.com>
2024-05-01 21:35:26 -0300

    Now when loading the data the component is shown loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { Suspense, lazy } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Loading from '@/pages/loading/Loading';
+import Chat from '@/pages/chat/Chat'
 
 const AppLayout = lazy(() => import('@/layouts/AppLayout'));
-const Chat = lazy(() => import('@/pages/chat/Chat'));
 const Description = lazy(() => import('@/pages/description/Description'));
 
 const App = () => {
